1d1b5785b54beb04d5e41febf46fc26406785a5c
Miles <miles@karmacheck.com>
2022-01-20 23:23:03 -0600

    a

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-from pickle import TRUE
 from pprint import pprint
 
 #rank words
@@ -70,9 +69,11 @@ def filterWords(words, greens, yellows, grays):
 
 
 words = getWords()
-# greens = ['', '', '', '', '']
-# yellows = [[''], [''], [''], [''], ['']]
-# grays = []
+greens = ['', '', '', '', '']
+yellows = [[''], [''], [''], [''], ['']]
+grays = []
+
+# print(len(filterWords(words, greens, yellows, grays)))
 
 #ROBOT 215 4/6
 # greens = ['', 'o', '', 'o', '']
@@ -103,16 +104,65 @@ words = getWords()
 # yellows = [[''], [''], [''], ['r'], ['']]
 # grays = ['a', 't', 'o', 'n', 's', 'h', 'p', 'd', 'c']
 
-greens = ['', '', '', '', '']
-yellows = [[''], [''], [''], [''], ['']]
-grays = []
+# greens = ['', '', '', 'e', '']
+# yellows = [['h', 's'], ['e', 'h'], ['r'], ['r'], ['e', 's']]
+# grays = ['a', 't', 'o', 'n', 'i']
 
-filtered = filterWords(words, greens, yellows, grays)
+#double letter, one gray, one yellow
+
+# filtered = filterWords(words, greens, yellows, grays)
 # pprint(filtered)
 
-pprint(filtered[:50])
+# pprint(filtered[:50])
 
 # print(len(filtered))
+
+
+def solve(solution):
+    greens = ['', '', '', '', '']
+    yellows = [[], [], [], [], []]
+    grays = []
+    answer = None
+    suggestions = getWords()
+    counter = 1
+    while answer == None:
+        counter += 1
+
+        suggestions = filterWords(suggestions, greens, yellows, grays)
+        if len(suggestions) == 1:
+            answer = suggestions[0]
+        guess = suggestions[0]['word']
+
+        for i in range(5):
+            letter = guess[i]
+            if letter not in solution:
+                grays.append(letter)
+                continue
+            if letter == solution[i]:
+                greens[i] = letter
+            else:
+                yellows[i].append(letter)
+        
+    return counter
+
+
+words = []
+with open('five-letter-words.json', "r") as fw:
+    words = json.load(fw)
+
+guesses = 0
+puzzles = 0
+for word in words['words'][:500]:
+    score = solve(word)
+    guesses += score
+    puzzles += 1
+
+
+print('guess average')
+print(guesses / puzzles)
+
+
+
 
 
     
